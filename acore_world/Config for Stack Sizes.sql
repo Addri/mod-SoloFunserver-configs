188e2d1a361f01f1da0837f706a8e9340a0c5bf4
Addri <lemonkey1988@gmail.com>
2023-11-01 11:04:49 +0000

    Tidying structure

--- acore_world/Config for Stack Sizes.sql
+++ acore_world/Config for Stack Sizes.sql
@@ -1,27 +1,32 @@
--- Change all trade goods to stack to 200
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 1536 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 1224 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 1024 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 512 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 144 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 128 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 80 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 72 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 64 AND `stackable` = 10;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 32 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 16 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `BagFamily` = 8 AND `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `class` = 7 AND `subclass` = 5;
-UPDATE `item_template` SET `stackable` = 200 WHERE `stackable` = 20;
-UPDATE `item_template` SET `stackable` = 200 WHERE `stackable` = 12;
-UPDATE `item_template` SET `stackable` = 200 WHERE `stackable` = 10;
-UPDATE `item_template` SET `stackable` = 200 WHERE `stackable` = 5;
+-- Change all trade goods to stack to 2000
+UPDATE `item_template` SET `stackable` = 2000 WHERE `BagFamily` IN (1536, 1224, 1024, 512, 232, 200, 144, 128, 80, 72, 64, 32, 16, 8);
+UPDATE `item_template` SET `stackable` = 2000 WHERE `class` = 7 AND `subclass` = 5;
+UPDATE `item_template` SET `stackable` = 2000 WHERE `stackable` IN (20, 12, 10, 5);
 
 -- Change all Repeatable farmable quest items to stack to 2000 for more convinient farming
-UPDATE `item_template` SET `stackable` = 2000 WHERE `entry` = 22527;
-UPDATE `item_template` SET `stackable` = 2000 WHERE `entry` = 22529;
-UPDATE `item_template` SET `stackable` = 2000 WHERE `entry` = 22528;
-UPDATE `item_template` SET `stackable` = 2000 WHERE `entry` = 18945;
+UPDATE `item_template` SET `stackable` = 2000 WHERE `entry` IN (22527, 22529, 22528, 18945);
+
+-- Change all Raid Token drops stack to 10 for convinient instance farming
+-- T3 - Naxxramas
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 22349 AND 22372;
+-- T4 - Karazhan, Gruuls Lair and Magtheridons Lair
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 29753 AND 29767;
+-- T5 - Serpenshring Cavern and The Eye
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 30236 AND 30250;
+-- T6 - Hyjal Summin, Black Temple and Sunwell Plateau
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 31089 AND 31103;
+-- T7 - Naxxramas (lv80) and Obsidian Santum
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 40610 AND 40639;
+-- T7 - Ulduar
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 45632 AND 45661;
+-- T8 - Trial of the Crusader
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` = 47242;
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 47557 AND 47559;
+-- T10 - Icecrown Citadel
+UPDATE `item_template` SET `stackable` = 10 WHERE `entry` BETWEEN 52025 AND 52030;
 
 -- Make Mithril Spurs Stackable to 20
 UPDATE `item_template` SET `stackable` = 20 WHERE `entry` = 7969;
+
+-- Change BOP to BOE
+UPDATE `item_template` SET `bonding` = 2 WHERE `bonding` = 1;
